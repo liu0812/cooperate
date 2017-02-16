7fc386557532f44c014c5d07c1e88014545c5af0
cyhdouyou1104 <1281206581@qq.com>
2017-02-16 10:55:50 +0800

    work

--- work.cpp
+++ work.cpp
@@ -1,7 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<conio.h>
+#include<windows.h>
 int a,b,c,d,jieguo,daan,i=0,j=0; 
+bool english=0; 
 struct fenshu
 {
 	int fenzi;
@@ -27,10 +30,24 @@ void jiafa()                         //加法函数
 	daan=a+b; 
 	scanf("%d",&jieguo);
 	if(daan==jieguo)
-	{printf("正确\n");
+	{if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
 	i++;}
 	else
-	{printf("错误\n");
+	{if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
 	j++;}
 }  
 void jianfa()                         //减法函数 
@@ -42,10 +59,24 @@ void jianfa()                         //减法函数
 	daan=a-b; 
 	scanf("%d",&jieguo);
 	if(daan==jieguo)
-	{printf("正确\n");
+	{if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
 	i++;}
 	else
-	{printf("错误\n");
+	{if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
 	j++;}
 }  
 void chengfa()                       //乘法函数 
@@ -57,10 +88,24 @@ void chengfa()                       //乘法函数
 	daan=a*b; 
 	scanf("%d",&jieguo);
 	if(daan==jieguo)
-	{printf("正确\n");
+	{if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
 	i++;}
 	else
-	{printf("错误\n");
+	{if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
 	j++;}
 }  
 void chufa()                         //除法函数 
@@ -73,10 +118,146 @@ void chufa()                         //除法函数
 	h=((int)(((float)a/b)*100+0.5))/100.0;
 	scanf("%f",&s);
 	if(h==s)
-	{printf("正确\n");
+	{if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
 	i++;}
 	else
-	{printf("错误\n");
+	{if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
+	j++;}
+}  
+void hunhe1()                      //混合运算模板1 
+{
+	srand(time(0));
+	a=rand()%10;
+	b=rand()%10;
+	c=rand()%10;
+	d=rand()%10;
+	printf("(%d+%d)*(%d-%d)=",a,b,c,d);
+	daan=(a+b)*(c-d); 
+	scanf("%d",&jieguo);
+	if(daan==jieguo)
+	{if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
+	i++;}
+	else
+	{if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
+	j++;}
+}  
+void hunhe2()                       //混合运算模板2 
+{
+	srand(time(0));
+	a=rand()%10;
+	b=rand()%10;
+	c=rand()%10;
+	d=rand()%10;
+	printf("(%d+%d)-(%d-%d)=",a,b,c,d);
+	daan=(a+b)-(c-d); 
+	scanf("%d",&jieguo);
+	if(daan==jieguo)
+	{if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
+	i++;}
+	else
+	{if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
+	j++;}
+}  
+void hunhe3()                         //混合运算模板3 
+{
+	srand(time(0));
+	a=rand()%10;
+	b=rand()%10;
+	c=rand()%10;
+	printf("%d+%d*%d=",a,b,c);
+	daan=a+b*c; 
+	scanf("%d",&jieguo);
+	if(daan==jieguo)
+	{if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
+	i++;}
+	else
+	{if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
+	j++;} 
+}  
+void hunhe4()                    //混合运算模板4 
+{
+	srand(time(0));
+	a=rand()%10;
+	b=rand()%10;
+	c=rand()%10;
+	printf("%d+%d-%d=",a,b,c);
+	daan=a+b-c; 
+	scanf("%d",&jieguo);
+	if(daan==jieguo)
+	{if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
+	i++;}
+	else
+	{if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
 	j++;}
 }  
 void fenshujiafa()                    //分数加法 
@@ -97,12 +278,26 @@ void fenshujiafa()                    //分数加法
 		result1=w/e;
 		scanf("%d",&qq);
 		if(qq==result1)
-		{printf("正确！\n");
+		{if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
 		i++;
 		}
 		else
 		{
-			printf("错误！\n");
+		if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
 			j++;
 		}
 	}
@@ -113,12 +308,26 @@ void fenshujiafa()                    //分数加法
 		scanf("%d %d",&qq,&ww);
 		if(qq==result1&&ww==result2)
 		{
-			printf("正确！\n");
+			if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
 			i++; 
 		}
 		else
 		{
-			printf("错误！\n");
+		if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
 			j++;
 		}
 	}
@@ -142,12 +351,26 @@ void fenshujianfa()                   //分数减法
 		result1=w/e;
 		scanf("%d",&qq);
 		if(qq==result1)
-		{printf("正确！\n");
+		{if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
 		i++;
 		}
 		else
 		{
-			printf("错误！\n");
+			if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
 			j++;
 		}
 	}
@@ -158,12 +381,26 @@ void fenshujianfa()                   //分数减法
 		scanf("%d %d",&qq,&ww);
 		if(qq==result1&&ww==result2)
 		{
-			printf("正确！\n");
+			if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
 			i++; 
 		}
 		else
 		{
-			printf("错误！\n");
+			if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
 			j++;
 		}
 	}
@@ -187,12 +424,26 @@ void fenshuchengfa()                    //分数乘法
 		result1=w/e;
 		scanf("%d",&qq);
 		if(qq==result1)
-		{printf("正确！\n");
+		{if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
 		i++;
 		}
 		else
 		{
-			printf("错误！\n");
+			if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
 			j++;
 		}
 	}
@@ -203,12 +454,26 @@ void fenshuchengfa()                    //分数乘法
 		scanf("%d %d",&qq,&ww);
 		if(qq==result1&&ww==result2)
 		{
-			printf("正确！\n");
+			if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
 			i++; 
 		}
 		else
 		{
-			printf("错误！\n");
+			if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
 			j++;
 		}
 	}
@@ -232,12 +497,26 @@ void fenshuchufa()                      //分数除法
 		result1=w/e;
 		scanf("%d",&qq);
 		if(qq==result1)
-		{printf("正确！\n");
+		{if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
 		i++;
 		}
 		else
 		{
-			printf("错误！\n");
+			if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
 			j++;
 		}
 	}
@@ -248,15 +527,134 @@ void fenshuchufa()                      //分数除法
 		scanf("%d %d",&qq,&ww);
 		if(qq==result1&&ww==result2)
 		{
-			printf("正确！\n");
+			if(english==0)
+	{
+		printf("正确！\n");
+	}
+	else
+	{
+		printf("RIGHT!\n");
+	}
 			i++; 
 		}
 		else
 		{
-			printf("错误！\n");
+			if(english==0)
+	{
+		printf("错误！\n");
+	}
+	else
+	{
+		printf("WRONG!\n");
+	}
 			j++;
 		}
 	}
 
 }
-
+int main()
+{
+	int e,k;
+	int nSelect=MessageBox(NULL,TEXT("您好,单击确定选择中文，click cancle switch to English"),TEXT("语言设置"),MB_OKCANCEL);
+	if(nSelect==IDOK)
+	{
+	printf("欢迎使用口算心算天天练！\n输入“e”来停止。\n说明：除法保留两位小数,分数运算依次输入分子分母中间用空格隔开。除号为'÷',分数线为'/'。\n"); 
+	printf("**********************************************\n");
+	for(k=0;1<4;k++)
+	{
+			srand(time(0));
+			e=rand()%12+1;
+			char o;
+		switch (e)
+	{
+		case 1:
+		jiafa();break;
+		case 2:
+		jianfa();break;
+		case 3:
+		chengfa();break;
+		case 4:
+		chufa();break;
+		case 5:
+		hunhe1();break;
+		case 6:
+		hunhe2();break;
+		case 7:
+		hunhe3();break;
+		case 8:
+		hunhe4();break;
+		case 9:
+		fenshujiafa();break;
+		case 10:
+		fenshujianfa();break;
+		case 11:
+		fenshuchengfa();break;
+		case 12:
+		fenshuchufa();break;
+	}
+	o=getchar();
+	if(o=='e'){
+	
+		printf("**********************************************\n");
+		printf("本次测试做对%d道题目\n",i);
+		printf("本次测试做错%d道题目\n\n",j-1);
+		printf("有志者事竟成！\n");
+		break;
+}
+	}
+	
+	
+	}
+	else
+	{
+		english=1;
+		printf("Welcome!\ninput“e”to stop.\n Notes:divison operation keep two places of decimal,fraction operation please input numerator than input denominator and space between them.The sign of divisor is '÷',the sign of fraction is'/'.\n"); 
+		printf("**********************************************\n");
+	for(k=0;1<4;k++)
+	{
+			srand(time(0));
+			e=rand()%12+1;
+			char o;
+		switch (e)
+	{
+		case 1:
+		jiafa();break;
+		case 2:
+		jianfa();break;
+		case 3:
+		chengfa();break;
+		case 4:
+		chufa();break;
+		case 5:
+		hunhe1();break;
+		case 6:
+		hunhe2();break;
+		case 7:
+		hunhe3();break;
+		case 8:
+		hunhe4();break;
+		case 9:
+		fenshujiafa();break;
+		case 10:
+		fenshujianfa();break;
+		case 11:
+		fenshuchengfa();break;
+		case 12:
+		fenshuchufa();break;
+	}
+	o=getchar();
+	if(o=='e'){
+	
+		printf("**********************************************\n");
+		printf("the number of right answer is %d in this quiz\n",i);
+		printf("the number of wrong answer is %d in this quiz\n\n",j-1);
+		printf("where there is a will there is a way！\n"); 
+		break;
+}
+	}
+	 
+	}
+	 getch();
+	 return 0;  
+	
+ } 
